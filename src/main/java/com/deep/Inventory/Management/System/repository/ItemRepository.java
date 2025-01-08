466752dfbab54156273acfa0526f7491fe3f2957
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2025-01-08 16:01:06 -0500

    Defining Entity and Repository

--- src/main/java/com/deep/Inventory/Management/System/repository/ItemRepository.java
+++ src/main/java/com/deep/Inventory/Management/System/repository/ItemRepository.java
@@ -2,7 +2,6 @@ package com.deep.Inventory.Management.System.repository;
 
 import com.deep.Inventory.Management.System.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
