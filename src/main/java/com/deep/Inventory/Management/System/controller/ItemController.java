eebd76fd46813b3989f61e692a2c0281020dd9bc
Deep-20 <49397575+Deep-20@users.noreply.github.com>
2025-01-09 14:21:53 -0500

    Adding endpoint for update and delete mapping

--- src/main/java/com/deep/Inventory/Management/System/controller/ItemController.java
+++ src/main/java/com/deep/Inventory/Management/System/controller/ItemController.java
@@ -32,4 +32,25 @@ public class ItemController {
         return itemService.addItem(item);
     }
 
+    @PutMapping("/add-quantity/{itemId}")
+    public Item addQuantity(@RequestBody Item item, @PathVariable int itemId){
+        return itemService.addQuantity(itemId, item.getQuantity());
+    }
+
+    @PutMapping("/reduce-quantity/{itemId}")
+    public Item reduceQuantity(@RequestBody Item item, @PathVariable int itemId){
+        return itemService.reduceQuantity(itemId, item.getQuantity());
+    }
+
+    @PutMapping("/update-price/{itemId}")
+    public Item updatePrice(@RequestBody Item item, @PathVariable int itemId){
+        return itemService.updatePrice(item.getPrice(), itemId);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public void deleteItem(@PathVariable int itemId){
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new RuntimeException("Item not found"));
+        itemService.deleteItemById(itemId);
+    }
+
 }
--- src/main/java/com/deep/Inventory/Management/System/service/ItemService.java
+++ src/main/java/com/deep/Inventory/Management/System/service/ItemService.java
@@ -23,4 +23,35 @@ public class ItemService {
     public Item getItemById(int id){
         return itemRepository.findById(id).orElseThrow(() -> new RuntimeException("Item Not Found"));
     }
+
+    public Item addQuantity(int itemId, int quantityToAdd){
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new RuntimeException("Item Not Found"));
+        item.setQuantity(item.getQuantity() + quantityToAdd);
+        itemRepository.save(item);
+        return item;
+    }
+
+    public Item reduceQuantity(int itemId, int quantityToReduce){
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new RuntimeException("Item Not Found"));
+        if(item.getQuantity() - quantityToReduce < 0){
+            throw new RuntimeException("Quantity to Reduce cannot be greater than current Quantity");
+        }
+        item.setQuantity(item.getQuantity() - quantityToReduce);
+        itemRepository.save(item);
+        return item;
+    }
+
+    public void deleteItemById(int itemId){
+        itemRepository.deleteById(itemId);
+    }
+
+    public Item updatePrice(int price, int itemId) {
+        Item item = itemRepository.findById(itemId).orElseThrow(() -> new RuntimeException("Item Not Found"));
+        if(price <= 0){
+            throw new RuntimeException("Price cannot be less than or equal to 0");
+        }
+        item.setPrice(price);
+        itemRepository.save(item);
+        return item;
+    }
 }
